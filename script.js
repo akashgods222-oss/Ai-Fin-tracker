f7b6838d89d79be4ce648568b7acdab43a95406e
akashgods222-oss <akashgods222@gmail.com>
2026-07-11 22:55:29 +0530

    Refactor expense logic and update AI status messages

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
 // ==========================================
 // 💸 FINTRACK CORE CORE LOGIC ENGINE
 // ==========================================
+document.addEventListener("DOMContentLoaded",() => {
+    
+
+
+aiStatusElement=document.getElementById("ai-status");
+myBtn=document.getElementById("analyze-btn");
 
 // 1. CONST Variables (Jo rules ya user profile fix hain)
 const userName = "Akash";
 const monthlyBudget = 15000; // Total pocket money/income
 
-// 2. LET Variables (Kharcha badalta rehta hai)
-let foodExpense = 4500;
+// 
+
+myBtn.addEventListener("click",() => {
+    let foodExpense = 4500;
 let internetRecharge = 500;
 let codingCourse = 2000;
-let roomRent = 6000;
+let roomRent = 3000;
 
-// Total calculation
+
 let totalExpenses = foodExpense + internetRecharge + codingCourse + roomRent;
-
-// 3. AI LOGIC IMPLEMENTATION (Decision Making)
-let aiRecommendation = "";
 
 // Triggering conditions
 if (totalExpenses > monthlyBudget) {
-    aiRecommendation = "🚨 ALERT " + userName + "! Tumne budget cross kar diya hai. Kharcha band karo boss!";
+    aiStatusElement.innerText="🚨 ALERT: Budget Khatam! Kharcha band karo.";
+    aiStatusElement.style.color="red"
 } else if (totalExpenses >= (monthlyBudget * 0.8)) {
     // Agar kharcha budget ke 80% ke barabar ya upar pohoch gaya hai
-    aiRecommendation = "⚠️ WARNING: Tumhara 80% budget khatam ho chuka hai. Bach ke chalo!";
+    aiStatusElement.innerText = "⚠️ WARNING: 80% Budget khatam ho chuka hai!";
+    aiStatusElement.style.color = "yellow"; 
 } else {
-    aiRecommendation = "✅ GOOD JOB " + userName + "! Tumhara kharcha control me hai. AI approves your savings! 💰";
+    aiStatusElement.innerText = "✅ GOOD JOB: Paisa control me hai!";
+    aiStatusElement.style.color = "green";
 }
-
+  
 // 4. TESTING AND DISPLAYING IN CONSOLE
-console.log("=== 🧾 FINTRACK REPORT ===");
-console.log("User:", userName);
-console.log("Budget:", "₹" + monthlyBudget);
-console.log("Total Spent:", "₹" + totalExpenses);
-console.log("AI Status:", aiRecommendation);
+//console.log("=== 🧾 FINTRACK REPORT ===");
+//console.log("User:", userName);
+//console.log("Budget:", "₹" + monthlyBudget);
+//console.log("Total Spent:", "₹" + totalExpenses);
+//console.log("AI Status:", aiStatusElement);
+  
+    });
+ });
 
